33bb6d29a72ba92d02f231ce36885b9f4ec14a9f
MANOJ KUMAR <manojkumar06t@gmail.com>
2024-06-14 10:54:50 +0530

    added config file containing json data

---
+++ src/assets/Data/config.js
@@ -0,0 +1,75 @@
+
+export const fileStructure = [
+    {
+      name: "public",
+      type: "folder",
+      children: [
+        {
+          name: "vite.svg",
+          type: "file",
+        },
+      ],
+    },
+    {
+      name: "src",
+      type: "folder",
+      children: [
+        {
+          name: "assets",
+          type: "folder",
+          children: [
+            {
+              name: "react.svg",
+              type: "file",
+            },
+          ],
+        },
+        {
+          name: "test",
+          type: "folder",
+          children: [],
+        },
+        {
+          name: "App.css",
+          type: "file",
+        },
+        {
+          name: "App.tsx",
+          type: "file",
+        },
+        {
+          name: "index.css",
+          type: "file",
+        },
+        {
+          name: "main.tsx",
+          type: "file",
+        },
+        {
+          name: "vite-env.d.ts",
+          type: "file",
+        },
+      ],
+    },
+    {
+      name: ".eslintrc.cjs",
+      type: "file",
+    },
+    {
+      name: ".gitignore",
+      type: "file",
+    },
+    {
+      name: "index.html",
+      type: "file",
+    },
+    {
+      name: "package-lock.json",
+      type: "file",
+    },
+    {
+      name: "package.json",
+      type: "file",
+    },
+  ];
+  
